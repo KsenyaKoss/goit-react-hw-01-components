a326551174c08999e17af7acbaf2a20676ddf680
KsenyaKoss <roxenn34@gmail.com>
2023-04-12 20:19:07 +0200

    prop-types-statistics

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,19 +1,31 @@
-import css from './Statistics.module.css'
+import css from './Statistics.module.css';
+import PropTypes from 'prop-types';
 
-
-
-export const Statistics = ({title, stats }) => {
-    return (
-        <section className={css.statistics}>
-         { title && <h2 className="title">{title}</h2> }
-        <ul className={css.stat_list}>
-        {stats.map(({id, label , percentage}) => {
-           return (<li key={id}>
-            <span className="label">{label}</span>
-            <span className="percentage">{percentage}</span>
-            </li>) 
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className="title">{title}</h2>}
+      <ul className={css.stat_list}>
+        {stats.map(({ id, label, percentage }) => {
+          return (
+            <li key={id}>
+              <span className="label">{label}</span>
+              <span className="percentage">{percentage}</span>
+            </li>
+          );
         })}
-        </ul>
-      </section>
-    )
-}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      label: PropTypes.string,
+      percentage: PropTypes.number,
+    })
+  ),
+};
